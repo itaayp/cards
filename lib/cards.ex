f660944e7130ccdd5751ac3fe97915cc44452767
Itay Peceniski <itay.peck@gmail.com>
2020-08-15 09:27:13 -0300

    my first commit

---
+++ lib/cards.ex
@@ -0,0 +1,20 @@
+defmodule Cards do
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits do
+      suit
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+end
